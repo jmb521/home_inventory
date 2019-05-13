0f7866cf267dfc957f3142a252c3ee8376d112ba
Jennifer Pazos <jmp521@gmail.com>
2019-05-13 11:24:15 -0500

    updated schema

--- db/migrate/20190412161725_create_items.rb
+++ db/migrate/20190412161725_create_items.rb
@@ -2,10 +2,9 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :name
-      t.string :purchase_date
-      t.string :date
-      t.string :estimated_replacement_date
-      t.string :approximate_value
+      t.date :purchase_date
+      t.float :purchase_price
+      t.string :store_purchased_from      
 
       t.timestamps null: false
     end
